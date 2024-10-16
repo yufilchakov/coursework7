00fa8789a38c7f3f984ca545fb6bbab7e1505dcd
yura_filchakov <filchakov1993@gmail.com>
2024-10-17 01:32:17 +0700

    paginators.py

---
+++ habit/paginators.py
@@ -0,0 +1,8 @@
+from rest_framework.pagination import PageNumberPagination
+
+
+class HabitPagination(PageNumberPagination):
+    """Пагинация для списка привычек."""
+    page_size = 5
+    page_size_query_param = 'page_size'
+    max_page_size = 100
